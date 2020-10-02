66c686722810ba76a144956452bebb9e7dcd07a2
bronigege <bronigege@gmail.com>
2020-10-03 00:47:30 +0200

    Incluyendo elemento de mensajes

--- index.ts
+++ index.ts
@@ -1,15 +1,1 @@
-import { Observable } from "rxjs/internal/Observable";
-
 export * from './core';
-
-function prueba(): Observable<string> {
-    return new Observable<string>((observer) => {
-        setInterval(() => {
-            observer.next('hola');
-        }, 1000);
-    })
-}
-
-prueba().subscribe(
-    (response: string) => console.log(response)
-);
