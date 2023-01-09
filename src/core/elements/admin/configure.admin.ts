7f3703598a385eaf09d23bf122e097d8d77cb6aa
bronigege <bronigege@gmail.com>
2023-01-09 08:39:08 +0100

    Incluye administración genral proyectos

--- src/core/elements/admin/configure.admin.ts
+++ src/core/elements/admin/configure.admin.ts
@@ -2,6 +2,7 @@ export class ConfigureAdmin {
     constructor() {
         this.change_icons()
         this.check_input_time()
+        this.load_image()
     }
 
     change_icons() {
@@ -45,6 +46,36 @@ export class ConfigureAdmin {
                     container.appendChild(item)
                 }
             }
+        }
+    }
+
+    load_image() {
+        const input = document.querySelector('input[type=file]')
+
+        if (input) {
+            const id = input.getAttribute('data-id')
+
+            input.addEventListener('change', () => {
+                console.log('change')
+
+                const reader = new FileReader()
+
+                reader.addEventListener('load', () => {
+                    if (id) {
+                        const image = document.getElementById(id)
+
+                        if (image) {
+                            // @ts-ignore
+                            image.src = reader.result
+                        }
+                    }
+                    console.log(reader.result)
+
+                })
+
+                // @ts-ignore
+                reader.readAsDataURL(input.files[0])
+            })
         }
     }
 }
